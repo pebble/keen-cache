71fb642dc7dc8d884cc60dfcf1e37bf29ab0d451
Thomas Sarlandie <thomas@sarlandie.net>
2014-02-18 15:46:01 -0800

    add powerful scrubland in the list of authorized domains

--- config.js
+++ config.js
@@ -3,7 +3,8 @@ var config = {};
 config.allowedDomains = [
   'http://localhost',
   'http://pebbleanalytics.localhost',
-  'http://pebbleanalytics.localhost'
+  'http://pebbleanalytics.localhost',
+  'http://powerful-scrubland-8520.herokuapp.com'
 ];
 
 module.exports = config;
