d2634587de003663c81d985e54d0254c8ac8982c
Thomas Sarlandie <thomas@sarlandie.net>
2014-02-19 18:59:16 -0800

    Now support setting a public scoped key and checking all params in it

--- proxy.js
+++ proxy.js
@@ -1,20 +1,28 @@
+var _ = require('underscore');
 var express = require('express');
 var logfmt = require('logfmt');
 var http = require('http');
+var querystring = require('querystring');
 var MongoClient = require('mongodb').MongoClient;
-var _ = require('underscore');
-var config = require('./config');
+var Keen = require('keen.io');
 
 var KEEN_SERVER = "http://api.keen.io";
-var mongoUri = process.env.MONGOLAB_URI || process.env.MONGOHQ_URL || 'mongodb://localhost/keen-cache';
+
+var config = {
+  // URL of your MongoDB database
+  mongoUri: process.env.MONGOLAB_URI || process.env.MONGOHQ_URL || 'mongodb://localhost/keen-cache',
+
+  // List of domains that will be "allowed" (if the browser respect CORS) to query this proxy
+  allowedDomains: JSON.parse(process.env.ALLOWED_DOMAINS),
+
+  // A new "master key" that you generate for your proxy
+  // > require('crypto').randomBytes(16).toString('hex');
+  publicKey: process.env.KEEN_PROXY_MASTER_KEY,
+  masterKey: process.env.KEEN_MASTER_KEY
+};
 
 
 var mongoDb;
-
-  // db.collection('mydocs', function(er, collection) {
-  //   collection.insert({'mykey': 'myvalue'}, {safe: true}, function(er,rs) {
-  //   });
-  // });
 
 var app = express();
 
@@ -47,16 +55,58 @@ var allowCrossDomain = function(req, res, next) {
 var cacheLookup = function(req, res, next) {
   mongoDb.collection('cache', function(er, collection) {
     // Get the most recent object from cache and serve it.
-    collection.findOne({ 'request': req.url }, { 'sort': {'cachedAt': -1} }, function(err, item) {
-      if (item) {
-        res.statusCode = 200;
-        res.send(item.response);
+
+    // Remove the api_key from the url we save in cache because it changes for every request.
+    var uniqueUrl = req.url.replace(/api_key=.*?&/, '');
+
+    collection.findOne({ 'request': uniqueUrl }, {}, { 'sort': { 'cachedAt': -1 }}, function(err, item) {
+      if (!err) {
+        if (item) {
+          res.statusCode = 200;
+          res.send(item.response);
+        }
+        else {
+          next();
+        }
       }
       else {
+        console.log("Error looking up item in cache: ", err);
         next();
       }
     });
   });
+}
+
+// This end point takes a public request and adds an authorization key
+var authorizePublicRequest = function(req, res, next) {
+  var publicScopedKey = req.query.api_key;
+
+  try {
+    var scopedParams = Keen.decryptScopedKey(config.publicKey, publicScopedKey);
+
+    // If we succesfully recognized the scope key, let's do some checks and then
+    // pass this on to Keen.io
+
+    // Prepare a new scoped key for Keen.io - We keep all the params in there, maybe
+    // some day soon Keen.io will support filtering on them too.
+    var privateScopedKey = Keen.encryptScopedKey(config.masterKey, scopedParams);
+    req.query.api_key = privateScopedKey;
+
+    // Overwrite the query parameters with the one set in the scopedKey
+    _.extend(req.query, scopedParams);
+
+    // Rewrite the filters a-la-mode Keen.io
+    req.query.filters = JSON.stringify(req.query.filters);
+
+    // Rewrite the URL with the new query parameters
+    req.url = req.path + '?' + querystring.stringify(req.query);
+
+    next();
+  }
+  catch (e) {
+    console.warn("Unable to decrypt scoped key. Rejecting request.");
+    res.send(403);
+  }
 }
 
 var proxyRequest = function(req, res, next) {
@@ -72,7 +122,9 @@ var proxyRequest = function(req, res, next) {
     });
     proxyResponse.on('end', function() {
       mongoDb.collection('cache', function(er, collection) {
-        collection.insert({ 'request': req.url, 'response': response, 'cachedAt': new Date() }, {}, function (er, rs) {
+        // Remove the api_key from the url we save in cache because it changes for every request.
+        var uniqueUrl = req.url.replace(/api_key=.*?&/, '');
+        collection.insert({ 'request': uniqueUrl, 'response': response, 'cachedAt': new Date() }, {}, function (er, rs) {
           if (er) {
             console.error("Error inserting data in cache - er=", er, " rs=", rs);
           }
@@ -81,6 +133,7 @@ var proxyRequest = function(req, res, next) {
       res.send(response);
     });
   }).on('error', function(error) {
+    res.statusCode = 500;
     res.send(JSON.encode({ 'error': error }));
   });
 }
@@ -88,13 +141,14 @@ var proxyRequest = function(req, res, next) {
 app.configure(function() {
   app.use(logfmt.requestLogger());
   app.use(allowCrossDomain);
+  app.use(authorizePublicRequest);
   app.use(cacheLookup);
   app.use(proxyRequest);
 });
 
 var port = Number(process.env.PORT || 5000);
 
-MongoClient.connect(mongoUri, function(err, db) {
+MongoClient.connect(config.mongoUri, function(err, db) {
   mongoDb = db;
 
   mongoDb.collection('cache', function(er, collection) {
@@ -109,6 +163,4 @@ MongoClient.connect(mongoUri, function(err, db) {
   app.listen(port, function() {
     console.log("Listening on port " + port);
   });
-  // db.log.events.ensureIndex( { "createdAt": 1 }, { expireAfterSeconds: 3600 } )
-
 });
